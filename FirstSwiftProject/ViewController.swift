4ab65a60d4c207cf31a0fca1f19201818fd50ab8
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-10-06 08:53:18 -0600

    I added an IB outlet and an IBaction to my program.

--- FirstSwiftProject/ViewController.swift
+++ FirstSwiftProject/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

cde241f2dc59164650e3ea5502cfee1c5d652fbe
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-10-06 09:22:15 -0600

    Made my background randomly change color when button was clicked.

--- FirstSwiftProject/ViewController.swift
+++ FirstSwiftProject/ViewController.swift
@@ -26,9 +26,22 @@ class ViewController: UIViewController
            }
         else
            {
-            firstButton.backgroundColor = .orange
+            firstButton.backgroundColor = .blue
            }
+            view.backgroundColor = createRandomColor()
     }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha:
+            CGFloat(1.0))
+        return newColor
+    }
+        
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()

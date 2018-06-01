d101baad4ac0df6708f2be1bd0957b81257f200d
wilshdog <31513793+wilshdog@users.noreply.github.com>
2018-06-01 07:35:27 -0600

    Final Commit

--- FirstSwiftProject/Controller/ViewController.swift
+++ FirstSwiftProject/Controller/ViewController.swift
@@ -33,11 +33,11 @@ class ViewController: UIViewController
     
     private func createRandomColor() -> UIColor
     {
-        let newColor :UIColor
+        
         let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
         let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
         let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
-        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha:
+        let newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha:
             CGFloat(1.0))
         return newColor
     }
